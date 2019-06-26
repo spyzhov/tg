4d1ea499c94bd256d4838ff93583625de0f958f3
Stepan Pyzhov <stepan.pyzhov@auto1.com>
2019-06-27 01:20:30 +0200

    fix go documentation

--- bot.go
+++ bot.go
@@ -1,12 +1,3 @@
-// Package telego implements Telegram Bot API
-//
-// > Generated: **27 June 2019**
-//
-// Please feel free to reed an original Telegram Bot API documentation: https://core.telegram.org/bots/api
-//
-// API for Bot was generated automatically using original documentation and may has the difference.
-// If this has happened, please inform the author with new issue https://github.com/spyzhov/telego/issues/new
-//
 package telego
 
 import (
